00d1f92e65520e52671ba60e0d068384cbcaf426
Mikko Marasigan <mkkmrsgn75@gmail.com>
2026-04-08 01:57:39 +0800

    Force IPv4 for email sending

--- backend/routes/issuer.js
+++ backend/routes/issuer.js
@@ -23,7 +23,10 @@ const {
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
